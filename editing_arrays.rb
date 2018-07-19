eb74e1b299f5b9872b9c4eff99af569b9f64fd0a
Christina Ng <christinasy317@gmail.com>
2018-07-19 18:36:31 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -11,4 +11,5 @@ end
 
 def add_colors
   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
+  return change_rainbow_colors.push("green", "blue")
 end
